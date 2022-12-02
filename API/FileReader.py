ae2c722321dd4906db26abbdd29000bdd73d9a23
AlexandreAANP <alexandreaanp@gmail.com>
2022-12-02 16:25:21 +0000

    FileReader updated add json test files

--- API/FileReader.py
+++ API/FileReader.py
@@ -1,4 +1,11 @@
+
 import json
+import sys
+import os
+
+sys.path.append(os.getcwd())
+from Objetos.Bicicleta import Bicicleta, Utilizador
+
 
 class FileReader:
     data = None
@@ -7,9 +14,27 @@ class FileReader:
     def __init__(self, path :str):
         self.data = json.loads(open(path).read())
         self.data_type = list(self.data.keys())[0]
-
-
-    def readTest():    
-        for object_data in self.data[self.data_type]:
+        if self.data_type == "Bicicleta":
+            self.readBicicleta()
+            return
+        if self.data_type == "Utilizador":
+            self.readUtilizador()
             
-                
+
+    def readUtilizador(self):
+        for object_data in self.data[self.data_type]:
+            object_data = list(object_data.values())
+            self.object_list.append(Utilizador(
+                object_data[0],
+                object_data[1],
+                object_data[2]
+            ))
+
+    def readBicicleta(self):    
+        for object_data in self.data[self.data_type]:
+            object_data = list(object_data.values())
+            self.object_list.append(Bicicleta(
+                object_data[0],
+                object_data[1],
+                object_data[2]
+            ))
--- API/api_bicicletas.py
+++ API/api_bicicletas.py
@@ -1,6 +1,6 @@
 from FileReader import FileReader
 import os
 
-FileReader(os.getcwd()+"/Data/teste.json")
+FileReader(os.getcwd()+"/Data/bicicleta.json")
 
 
