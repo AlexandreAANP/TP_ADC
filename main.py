885502f7612955363b54051a3e8f67f0a32b8978
Bruno Gonçalves <brgoncalves@deloitte.pt>
2022-12-02 10:33:17 +0000

    criação menu

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# 
+def menu():
+
+    print("""
+
+                        Bicicletas 
+    ------------------------------------------------------
+                1 - Inserir Bicicleta
+                2 - Eliminar Bicicleta
+                3 - Listar Bicicletas
+                4 -
+                5 - 
+                6 - Guardar
+                7 - Carregar tudo
+                0 - Sair
+    ------------------------------------------------------              
+    """)
+
+
+if __name__ == "__main__":
+
+    menu()
+
