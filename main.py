e55b41ec2fcbb5320575abe2795d31c4af53bdfd
Bruno Gonçalves <brgoncalves@deloitte.pt>
2022-12-02 10:38:12 +0000

    nova estrutura e class bicicletas

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 def menu():
-
-    print("""
-
-                        Bicicletas 
-    ------------------------------------------------------
-                1 - Inserir Bicicleta
-                2 - Eliminar Bicicleta
-                3 - Listar Bicicletas
-                4 -
-                5 - 
-                6 - Guardar
-                7 - Carregar tudo
-                0 - Sair
-    ------------------------------------------------------              
-    """)
+    print("Menu")
 
 
 if __name__ == "__main__":
---
+++ menu/menu.py
@@ -0,0 +1,16 @@
+def menu():
+
+    print("""
+
+                        Bicicletas 
+    ------------------------------------------------------
+                1 - Inserir Bicicleta
+                2 - Eliminar Bicicleta
+                3 - Listar Bicicletas
+                4 -
+                5 - 
+                6 - Guardar
+                7 - Carregar tudo
+                0 - Sair
+    ------------------------------------------------------              
+    """)
