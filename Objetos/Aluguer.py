3d802fcd51310da8ab3e53ca0bf44442deaf692a
Bruno Gonçalves <brgoncalves@deloitte.pt>
2022-12-22 23:10:08 +0000

    Documentação Aluguer

--- Objetos/Aluguer.py
+++ Objetos/Aluguer.py
@@ -18,11 +18,19 @@ class Aluguer:
     fileReader = None
 
     def __init__(self, bicicleta: Bicicleta, utilizador :Utilizador, id = None, writeFile = True):
-        """Bicicleta
+        """
+            Bicicleta
             No construtor, declaramos os atributos que definem o objeto
-            @param bicicleta: recebe uma bicicleta do tipo Bicicleta
-            @param utilizador: recebe um utilizador do tipo Utilizador 
-            @param id: não recebe nada
+
+            :param Bicicleta: recebe bicicleta
+            :type Bicicleta: bicicleta
+
+            :param Utilizador: recebe utilizador 
+            :type Utilizador: utilizador
+
+            :param id: recebe id
+            :type int: id
+
         """
 
         self.__bicicleta = bicicleta
@@ -48,8 +56,10 @@ class Aluguer:
     @property
     def id(self):
         """
-            Função para ir buscar o id do aluger
-            @return: id do aluguer
+            Metodo para ir buscar o id do aluger
+            
+            :return: id
+            :rtype: Aluguer or none
         """
         return self.__id
 
@@ -57,7 +67,9 @@ class Aluguer:
     def utilizador(self):
         """
             Metodo para apresentar o objeto Utilizador
-            @return: utilizador
+            
+            :return: utilizador
+            :rtype: utilizador
         """
         return self.__utilizador
 
@@ -65,21 +77,28 @@ class Aluguer:
     def bicicleta(self):
         """
             Metodo para apresentar o objeto Bicicletas
-            @return: Bicicleta
+            ´
+            :return: bicicleta
+            :rtype: bicicleta
         """
         return self.__bicicleta
 
     def __repr__(self):
         """
             Metodo que vai imprimir um Aluguer apresentando a bicicleta e o utilizador 
-            @return: Retorna uma String que representa a bicicleta
+            
+            :return: Retorna uma String que apresenta quem fez o aluguer
+            :rtype: Aluguer
+
         """
         return f'''Aluguer(\n\n{self.__id}, {self.__bicicleta}, {self.__utilizador} )'''
 
     def get_json_object(self):
         """
             Metodo para retornar o objeto da mesma maneira que é guardado no json em formato dict
-            @return: Retorna o dict
+            
+            :return: utilizador, bicicleta, id 
+            :rtype: dict
         """
         return {
             "utilizador" : [
@@ -100,19 +119,31 @@ class Aluguer:
 
     @classmethod
     def contains(cls, parameter):
+        """
+            Este metodo vai verificar se o parametro existe la lista
+
+            :return: retorna um aluger 
+            :rtype Aluger:
+        """
         return cls.Lista_Aluguers.__contains__(parameter)
 
-    # Ver isto melhor
+
     def get_aluguer_id(self):
         """
-            @return: 
+            Este metodo vai apresentar o id do objeto Aluguer
+            :return: id
+            :rtype int: 
         """
         return self.id
 
     @classmethod
     def random_create_aluguer(cls):
         """
-            Metodo para criar um aluguer com valores aleatóriosde 
+            Metodo para criar um aluguer com valores aleatórios
+
+            :return: id,bicicleta,utilizador
+            :rtype Aluguer:
+
         """
         aluguer = Aluguer(Bicicleta.get_random_bike(), Utilizador.get_random_user())
         print(f"Aluguer {aluguer.id}: {aluguer.utilizador.nome} alugou {aluguer.bicicleta.modelo}")
@@ -122,7 +153,7 @@ class Aluguer:
     @classmethod
     def random_delete_aluguer(cls):
         """
-            Metodo para eliminar um aluguer
+            Metodo para eliminar um aluguer 
         """
         aluguer = random.choice(Aluguer.Lista_Aluguers)
         print(f"Acabou o aluguer com o ID {aluguer.id} de {aluguer.utilizador.nome}. Durou {random.randint(0, 24)} horas.")
